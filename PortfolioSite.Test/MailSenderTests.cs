cb390dbac083bc1a31354a346859c7120b9d1d5f
lsubak <35020658+lsubak@users.noreply.github.com>
2020-09-04 19:09:53 -0400

    Fix portfolio overview responsive-ness

---
+++ PortfolioSite.Test/MailSenderTests.cs
@@ -0,0 +1,25 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace PortfolioSite.Test
+{
+    [TestClass]
+    public class MailSenderTests
+    {
+
+        [TestMethod]
+        public void MailSender_InvalidEmail_ReturnsInvalidEmailError()
+        {
+
+        }
+
+        [TestMethod]
+        public void MailSender_ValidEmail_ReturnsConfirmation()
+        {
+        }
+
+        [TestMethod]
+        public void MailSender_Exception_ReturnsError()
+        {
+        }
+    }
+}
